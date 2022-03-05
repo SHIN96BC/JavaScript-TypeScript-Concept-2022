91766588acc5e9d75181274747ea84638247379e
신병철 <mousecjf@gmail.com>
2022-03-05 21:34:31 +0900

    Add files via upload

---
+++ 09_Exception/09_Exception.js
@@ -0,0 +1,28 @@
+
+function doException() {
+    throw new Error('오류 발생');
+}
+
+function noException() {
+    return true;
+}
+
+function callException(type) {
+    if(type === 'do') {
+        doException();
+    } else {
+        noException();
+    }
+}
+
+function main() {
+    try {
+        callException('do');
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log('done');
+    }
+}
+
+main();
